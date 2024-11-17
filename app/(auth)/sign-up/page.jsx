3bfe46798fbb8e3229e4d05d1c2ac5acb7b70fb2
Sushi <shaheer.anser@gmail.com>
2024-11-17 16:57:20 +0500

    fixed vercel errors

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -37,7 +37,7 @@ export default function Signup() {
     if (isLoggedIn) {
       router.push("/");
     }
-  }, [isLoggedIn]);
+  }, [isLoggedIn, router]);
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
