c08446df1dbb453559f8322a4400c65684c00d21
FaizanHaider108 <fiver1faizan@gmail.com>
2024-11-30 19:17:23 +0500

    Added Our Story Layout

--- app/OurStory/layout.jsx
+++ app/OurStory/layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export const metadata = {
-    title: "Lookvisa - Privacy Policy",
-    description: "With LookVisa, is a platform matching immigrant investors with investment projects and opportunities across multiple countries to obtain an immigrant investor visa. With tools for uploading investor profile, listed investment projects and more, you can find an investment visa",
+    title: "LookVisa: Investment Visas – Find Immigrant Investor Visas – Our Story",
+    description: " With LookVisa, you can search for investment projects and opportunities to obtain an immigrant investor visa. With tools for uploading investor profile, listed investment projects and more, you can find an investment visa",
     keyword:[
       "investment visa, lookvisa, investment opportunities, foreign investor, immigrant investor, project funding, search engine platform for immigrant investment visas"
     ]
+    
   };
   
 const layout = ({children}) => {
