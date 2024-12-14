604b9805475851107f2aeac92aa28625860fef52
Sushi <shaheer.anser@gmail.com>
2024-12-14 23:24:50 +0500

    updated api route

--- app/api/edit-profile/route.js
+++ app/api/edit-profile/route.js
@@ -36,7 +36,6 @@ export const PUT = async (req) => {
         }
 
         if (targetUser.role === "Investor") {
-            console.log(body)
             const {
                 avatarURL,
                 firstName,
