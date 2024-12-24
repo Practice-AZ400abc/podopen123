67b4ec968bb2217faeceabb268e1b2700669c898
FaizanHaider108 <fiver1faizan@gmail.com>
2024-12-24 16:25:02 +0500

    Add status filter to Listings in Wrapper component for enhanced management

--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -2,7 +2,14 @@
 import Link from 'next/link'
 import React from 'react'
 import Listings from './Listings'
-
+import {
+    Select,
+    SelectContent,
+    SelectTrigger,
+    SelectValue,
+    SelectItem,
+} from "@/components/ui/select";
+import { Filter } from 'lucide-react';
 const Wrapper = () => {
     // getting Listings from backend
     return (
@@ -13,14 +20,44 @@ const Wrapper = () => {
                     <Link href='/' className='text-md underline'>Analytics</Link>
                     <Link href='/' className='text-md underline'>Your Listings</Link>
                 </ul>
-             </div>
-             {/* wrappeing Lisings */}
-             <div className='w-[90%] border rounded-md  bg-white  p-5'>
+            </div>
+            {/* wrappeing Lisings */}
+            <div className='w-[90%] border rounded-md  bg-white  p-5'>
+                <div className='flex items-start justify-between'>
                     <h1 className='text-4xl font-bold'>Your Listings</h1>
-                    <div className='mt-5'>
-                        <Listings />
+                    <div className='w-[200px]'>
+                        <Select>
+                            <SelectTrigger className="h-12">
+                                <div className="flex items-center gap-2">
+                                    <Filter />
+                                    <SelectValue
+                                        className="text-lg font-bold"
+                                        value={null} // Ensure no value is selected initially
+                                        placeholder="Status"
+                                    />
+                                </div>
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="active">
+                                    Active
+                                </SelectItem>
+                                <SelectItem value="inactive">
+                                    Inactive
+                                </SelectItem>
+                                <SelectItem value="pending">
+                                    Pending
+                                </SelectItem>
+                                <SelectItem value="sold_expired">
+                                    Sold / Expired
+                                </SelectItem>
+                            </SelectContent>
+                        </Select>
                     </div>
-             </div>
+                </div>
+                <div className='mt-5'>
+                    <Listings />
+                </div>
+            </div>
         </div>
     )
 }
