0b5e796dfa87d6645aef679f7aae4c9a7908a554
FaizanHaider108 <fiver1faizan@gmail.com>
2024-12-30 18:14:45 +0500

    style: increase font size for main heading in Home component; adjust Navbar link formatting

--- app/page.jsx
+++ app/page.jsx
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50 flex flex-col items-center pt-4 px-4">
       <div className="w-full max-w-3xl mx-auto text-center space-y-6 mt-6">
-        <h1 className="text-3xl  text-black font-bold">Get sponsorship for your golden, eb5, investor visa</h1>
+        <h1 className="text-4xl leading-10 text-black font-bold">Get sponsorship for your golden, eb5, investor visa</h1>
         <div className="flex gap-3 bg-white  items-center justify-center   p-3 rounded-md border border-blue-400 shadow-md">
           
           <Select onValueChange={(value) => setSelectedCountry(value)}>
